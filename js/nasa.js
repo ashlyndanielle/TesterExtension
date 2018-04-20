85f80b82685681ab9318536a555b250fece5dc67
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-04-20 14:27:18 -0600

    added caption and modified to show plus sign

--- js/nasa.js
+++ js/nasa.js
@@ -1,14 +1,23 @@
 
 window.onload = () => {
   $.get(apod)
-  .done( setBackgroundImage )
+  .done( setApodInfo )
   .fail( err => console.log(err) )
 }
 
 const apod = "https://api.nasa.gov/planetary/apod?" + $.param({
-  'api_key': config.nasaKey
+  'api_key': config.nasaKey,
+  'count': 5
 })
 
-function setBackgroundImage(data) {
-  $('#container').css('background-image', 'url("' + data.hdurl + '")');
+let author;
+let description;
+
+function setApodInfo(data) {
+  console.log(data);
+  // $('#container').css('background-image', 'url("' + data[0].url + '")');
+  $('#nasa-img').attr('src', data[0].url);
+  if (data[0].copyright) author = data[0].copyright;
+  description = data[0].explanation;
 }
+
