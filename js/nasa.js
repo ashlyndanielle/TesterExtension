90dcbd47f72fce9a3cfec76ba3b70672b3e75039
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-04-20 14:55:55 -0600

    nasa image hover with minified caption working properly

--- js/nasa.js
+++ js/nasa.js
@@ -1,3 +1,31 @@
+const apod = "https://api.nasa.gov/planetary/apod?" + $.param({
+  'api_key': config.nasaKey,
+  'count': 5
+});
+
+const nasaContainer = $('#container');
+const nasaImg = $('#nasa-img');
+const caption = $('.nasa-caption');
+
+let author;
+let description;
+
+nasaContainer.hover( () => {
+  setTimeout( () => {
+    caption.fadeIn();
+  }, 300)
+}, () => {
+  caption.hide();
+})
+
+caption.hover( () => {
+  $(this).style.color = 'red';
+}, () => {
+  $(this).style.color = 'white';
+})
+
+
+
 
 window.onload = () => {
   $.get(apod)
@@ -5,18 +33,10 @@ window.onload = () => {
   .fail( err => console.log(err) )
 }
 
-const apod = "https://api.nasa.gov/planetary/apod?" + $.param({
-  'api_key': config.nasaKey,
-  'count': 5
-})
-
-let author;
-let description;
-
 function setApodInfo(data) {
   console.log(data);
   // $('#container').css('background-image', 'url("' + data[0].url + '")');
-  $('#nasa-img').attr('src', data[0].url);
+  nasaImg.attr('src', data[0].url);
   if (data[0].copyright) author = data[0].copyright;
   description = data[0].explanation;
 }
