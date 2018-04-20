3f0849fde2966a2f6a59ded2fc2a941aaeb4bc9a
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-04-20 15:15:07 -0600

    caption hover working correctly and in better location

--- js/nasa.js
+++ js/nasa.js
@@ -1,43 +1,46 @@
-const apod = "https://api.nasa.gov/planetary/apod?" + $.param({
-  'api_key': config.nasaKey,
-  'count': 5
-});
+$(document).ready(function() {
 
-const nasaContainer = $('#container');
-const nasaImg = $('#nasa-img');
-const caption = $('.nasa-caption');
+  const apod = "https://api.nasa.gov/planetary/apod?" + $.param({
+    'api_key': config.nasaKey,
+    'count': 5
+  });
 
-let author;
-let description;
+  const nasaContainer = $('#container');
+  const nasaImg = $('#nasa-img');
+  const caption = $('.nasa-caption');
 
-nasaContainer.hover( () => {
-  setTimeout( () => {
-    caption.fadeIn();
-  }, 300)
-}, () => {
-  caption.hide();
+  let author;
+  let description;
+
+  nasaContainer.hover( () => {
+    setTimeout( () => {
+      caption.fadeIn();
+    }, 300)
+  }, () => {
+    caption.hide();
+  })
+
+  caption.hover( () => {
+    caption.css('color', 'red');
+  }, () => {
+    caption.css('color', 'white');
+  })
+
+
+
+
+  window.onload = () => {
+    $.get(apod)
+    .done( setApodInfo )
+    .fail( err => console.log(err) )
+  }
+
+  function setApodInfo(data) {
+    console.log(data);
+    // $('#container').css('background-image', 'url("' + data[0].url + '")');
+    nasaImg.attr('src', data[0].url);
+    if (data[0].copyright) author = data[0].copyright;
+    description = data[0].explanation;
+  }
+
 })
-
-caption.hover( () => {
-  $(this).style.color = 'red';
-}, () => {
-  $(this).style.color = 'white';
-})
-
-
-
-
-window.onload = () => {
-  $.get(apod)
-  .done( setApodInfo )
-  .fail( err => console.log(err) )
-}
-
-function setApodInfo(data) {
-  console.log(data);
-  // $('#container').css('background-image', 'url("' + data[0].url + '")');
-  nasaImg.attr('src', data[0].url);
-  if (data[0].copyright) author = data[0].copyright;
-  description = data[0].explanation;
-}
-
